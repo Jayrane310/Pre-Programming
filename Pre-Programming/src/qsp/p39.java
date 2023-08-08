193a9901d7117e1f1add4e6ec45fe4b520d3e930
Jayrane310 <123440077+Jayrane310@users.noreply.github.com>
2023-08-09 00:24:40 +0530

    Create p39.java
    adding new progtamme

---
+++ Pre-Programming/src/qsp/p39.java
@@ -0,0 +1,14 @@
+//write a program to display cities of Maharashtra using for each loop and create an array of those cities
+package qsp;
+
+public class p39 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a[]={"Mumbai","Pune","Nagpur","Nashik","Satara"};
+		for (String i : a) {
+			System.out.println(i);
+		}
+	}
+
+}
